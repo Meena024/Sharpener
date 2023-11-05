bf497b1e94bbb2a30a900f1a35f02521adb3ec04
Meena024 <maill2rmeena@gmail.com>
2023-11-05 10:40:35 +0530

    DOM Manipulations task 6 commit

--- 2.js
+++ 2.js
@@ -7,18 +7,44 @@
 // console.dir(document.all);
 // //console.dir(document.all[10].innerText="hello");
 
-var headertitle=document.getElementById('header-title');
-var header = document.getElementById('main-header');
-console.log(headertitle.textContent);
-header.style.borderBottom = 'solid 3px black';
-var title = document.getElementsByClassName('title');
-console.log(title[0].innerText);
-title[0].style.fontWeight='bold';
-title[0].style.color='green';
-var item = document.getElementsByClassName('list-group-item');
-item[2].style.background='green';
-var items = document.getElementById('items');
-items.style.fontWeight='bold';
-items.style.color='orange';
-var header = document.querySelector('#main-header');
-header.style.borderBottom = 'solid 3px black';
+// var headertitle=document.getElementById('header-title');
+// var header = document.getElementById('main-header');
+// console.log(headertitle.textContent);
+// header.style.borderBottom = 'solid 3px black';
+// var title = document.getElementsByClassName('title');
+// console.log(title[0].innerText);
+// title[0].style.fontWeight='bold';
+// title[0].style.color='green';
+// var item = document.getElementsByClassName('list-group-item');
+// item[2].style.background='green';
+// var items = document.getElementById('items');
+// items.style.fontWeight='bold';
+// items.style.color='orange';
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom = 'solid 3px black';
+
+// var input = document.querySelector('input');
+// input.value = 'Hello World!';
+
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value='SEND';
+
+// var item = document.querySelector('.list-group-item');
+// item.style.color='red';
+
+// var lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color='blue';
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.background='green';
+
+var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.display='none';
+
+var list = document.querySelectorAll('.list-group-item');
+list[1].style.color='darkgreen';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for(let i=0;i<odd.length;i++){
+        odd[i].style.backgroundColor='green';
+    }
