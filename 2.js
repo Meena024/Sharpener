b887e43675e50cb0c0a9d6b68241f33964527894
Meena024 <maill2rmeena@gmail.com>
2023-11-06 12:25:14 +0530

    DOM Manipulation task 7 commit

--- 2.js
+++ 2.js
@@ -35,16 +35,52 @@
 // var lastItem = document.querySelector('.list-group-item:last-child');
 // lastItem.style.color='blue';
 
-var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.background='green';
+// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.background='green';
 
-var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
-thirdItem.style.display='none';
+// var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.display='none';
 
-var list = document.querySelectorAll('.list-group-item');
-list[1].style.color='darkgreen';
+// var list = document.querySelectorAll('.list-group-item');
+// list[1].style.color='darkgreen';
 
-var odd = document.querySelectorAll('li:nth-child(odd)');
-for(let i=0;i<odd.length;i++){
-        odd[i].style.backgroundColor='green';
-    }
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// for(let i=0;i<odd.length;i++){
+//         odd[i].style.backgroundColor='green';
+//     }
+
+var itemList = document.querySelector('#items');
+
+// itemList.parentNode.style.backgroundColor='darkgrey';
+// console.log(itemList.parentNode);
+// console.log(itemList.parentNode.parentNode);
+// itemList.parentElement.style.backgroundColor='darkgrey';
+// console.log(itemList.parentElement);
+// console.log(itemList.parentElement.parentElement);
+
+//child nodes
+
+// console.log(itemList.childNodes);
+// console.log(itemList.children);
+// itemList.children[1].style.backgroundColor='yellow';
+// console.log(itemList.firstChild);
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent='Hello 1';
+
+var newDiv = document.createElement('div');
+newDiv.className='classndiv';
+newDiv.id='idnsdiv';
+newDiv.setAttribute('title','titlendiv');
+var newDivText = document.createTextNode('Hello');
+newDiv.appendChild(newDivText);
+newDiv.style.fontSize = '20px' ;
+console.log(newDiv);
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+container.insertBefore(newDiv,h1);
+
+var newItem = document.createElement("li");
+newItem.className = "list-group-item";
+newItem.textContent = 'Hello';
+itemList.insertBefore(newItem, itemList.firstChild);
+console.log(newItem);
